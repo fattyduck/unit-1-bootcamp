8de722b5212102be4133b40c5b69af29bbff604c
fattyduck <dison.ruan@yahoo.com>
2015-04-14 20:46:02 -0400

    finished methods

--- src/Methods.java
+++ src/Methods.java
@@ -6,8 +6,33 @@ public class Methods {
         int square=i*i;
         return square;
     }
+    public static double calculateSquareRoot(int i){
+        double root=Math.sqrt(i);
+        return root;
+    }
+    public static String toLower(String str){
+        String s = str.toLowerCase();
+        return s;
+    }
+    public static boolean isMultiple(int one, int two){
+        boolean b = two%one==0;
+        return b;
+    }
+    public static void prettyInt(int i){
+        String star="";
+        for(int j = 0; j<i;j++){
+            star+="*";
+        }
+        System.out.println(star+" "+i+" "+star);
+
+
+    }
 
   public static void main (String args[]) {
       System.out.println(calculateSquare(10));
+      System.out.println(calculateSquareRoot(10));
+      System.out.println(toLower("Sup, Cow?"));
+      System.out.println(isMultiple(5,7));
+      prettyInt(8);
   }
 }
